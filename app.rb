70d7ba00a39a67ed56e0597f380ddace3cf12a67
ashih92 <146589396+ashih92@users.noreply.github.com>
2023-12-08 16:33:03 +0000

    commit

--- app.rb
+++ app.rb
@@ -2,6 +2,7 @@ require "sinatra"
 require "sinatra/reloader"
 require "http"
 
+
 # define a route
 get("/") do
 
@@ -9,7 +10,9 @@ get("/") do
   api_url = "https://api.exchangerate.host/list?access_key=#{ENV["fa37e94f30043729bbef4e8a0bb21d65"]}"
 
   # use HTTP.get to retrieve the API information
+  #get request to the URL
   raw_data = HTTP.get(api_url)
+  require "json"
 
   # convert the raw request to a string
   raw_data_string = raw_data.to_s
@@ -18,17 +21,20 @@ get("/") do
   parsed_data = JSON.parse(raw_data_string)
 
   # get the symbols from the JSON
-  # @symbols = parsed_data ...
+  @symbols = parsed_data.fetch("Currency Symbols")
 
   # render a view template where I show the symbols
-  # erb(:homepage)
+  erb(:layout)
 end
+
+
 get("/:from_currency") do
   @original_currency = params.fetch("from_currency")
 
   api_url = "https://api.exchangerate.host/list?access_key=#{ENV["fa37e94f30043729bbef4e8a0bb21d65"]}"
   
   # some more code to parse the URL and render a view template
+  erb(:from)
 end
 
 get("/:from_currency/:to_currency") do
@@ -38,4 +44,5 @@ get("/:from_currency/:to_currency") do
   api_url = "https://api.exchangerate.host/convert?access_key=#{ENV["fa37e94f30043729bbef4e8a0bb21d65"]}&from=#{@original_currency}&to=#{@destination_currency}&amount=1"
   
   # some more code to parse the URL and render a view template
+  erb(:to)
 end
